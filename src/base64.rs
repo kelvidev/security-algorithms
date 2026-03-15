448d98686fd980c4b7ddd2f9cd2d2ff06789033c
kelvi <kelvi.carvalho@cs.up.edu.br>
2026-03-15 15:02:21 -0300

    add decode function

--- src/base64.rs
+++ src/base64.rs
@@ -1,8 +1,8 @@
 const BASE_64_CHARS: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
-pub fn encode(word: &str) -> String {
+pub fn encode(data: &str) -> String {
     let mut binary_sequence = String::new();
-    for simbol in word.chars() {
+    for simbol in data.chars() {
         let binary_value = format!("{:08b}", simbol as u32);
         binary_sequence += &binary_value;
     }
@@ -11,27 +11,53 @@ pub fn encode(word: &str) -> String {
     let mut encoded_value = String::new();
     let mut binary_group = String::new();
     for i in (0..=binary_sequence.len() - 6).step_by(6) {
-        binary_group += &binary_sequence[i..i + 6];
-        println!("{}", binary_group);
+        binary_group = String::from(&binary_sequence[i..i + 6]);
         let decimal_value: u8 = u8::from_str_radix(&binary_group, 2).unwrap();
         let base_64_value = BASE_64_CHARS.chars().nth(decimal_value as usize).unwrap();
         encoded_value.push(base_64_value);
         binary_group.clear();
     }
-    if word.len() % 3 == 1{
+    if data.len() % 3 == 1{
         let incomplete_binary = binary_sequence[binary_sequence.len()-2..binary_sequence.len()].to_string() + "0000";
         let decimal_value: u8 = u8::from_str_radix(&incomplete_binary, 2).unwrap();
         let base_64_value = BASE_64_CHARS.chars().nth(decimal_value as usize).unwrap();
         encoded_value.push(base_64_value);
         encoded_value += "==";
     }
-    else if word.len() % 3 == 2{
+    else if data.len() % 3 == 2{
         let incomplete_binary = binary_sequence[binary_sequence.len()-4..binary_sequence.len()].to_string() + "00";
-       println!("{}",incomplete_binary);
         let decimal_value: u8 = u8::from_str_radix(&incomplete_binary, 2).unwrap();
         let base_64_value = BASE_64_CHARS.chars().nth(decimal_value as usize).unwrap();
         encoded_value.push(base_64_value);
         encoded_value += "=";
     }
     return encoded_value;
+}
+
+pub fn decode(data: &str) -> String {
+
+    let equals_amount = data.matches('=').count();
+    let formated_data = data.replace("=", "");
+
+    let mut binary_sequence = String::new();
+
+    for simbol in formated_data.chars() {
+        let decimal_value: u8 = BASE_64_CHARS.find(simbol).unwrap() as u8; 
+        let binary_value = format!("{:06b}", decimal_value );
+        binary_sequence += &binary_value;
+    }
+    if equals_amount == 1{
+        binary_sequence.truncate(binary_sequence.len() - 2);
+    }
+    else if equals_amount == 2{
+        binary_sequence.truncate(binary_sequence.len() - 4);
+    }
+    let mut decoded_value:String = String::from("");
+    for i in (0..=binary_sequence.len() - 8).step_by(8) {
+        let binary = u8::from_str_radix(&binary_sequence[i..i+8], 2).unwrap();
+        decoded_value += &(binary as char).to_string();
+    }
+
+
+    return decoded_value;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod base64;
 
 fn main() {
-     let word = "thats a test";
-    println!("{}",base64::encode(word));
+    let word = "test value";
+    let encoded_value = base64::encode(word);
+    println!("encoded Value = {}",encoded_value);
+    print!("Decoded value = {}", base64::decode(&encoded_value));
 }
